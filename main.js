730fd10a5e9c231b98ff23b7032d7bfb25dc8f81
Jose Barrios <josebarrios@pursuit.org>
2023-11-18 15:44:58 -0500

    added random books on page load

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+function loadRandomBooks() {
+    const randomSearchTerms = [
+        "fantasy",
+        "history",
+        "science",
+        "fiction",
+        "mystery",
+        "romance",
+        "biography",
+        "self-help",
+        "travel",
+        "business",
+        "technology",
+        "philosophy",
+        "art",
+        "music",
+        "health",
+        "fitness",
+        "cooking",
+        "sports",
+        "education",
+        "psychology",
+        "comedy",
+        "drama",
+        "poetry",
+        "religion",
+        "nature",
+        "programming",
+        "design",
+        "mathematics",
+        "economics",
+        "politics",
+      ]
+
+    const randomIndex = Math.floor(Math.random() * randomSearchTerms.length)
+    const randomTerm = randomSearchTerms[randomIndex]
+
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${randomTerm}&maxResults=5&key=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => {
+        data.items.forEach((book) => displayBook(book))
+      })
+}
+
+loadRandomBooks()
+
 let form = document.querySelector("form")
 
 form.addEventListener("submit", (event) => {
@@ -37,17 +83,27 @@ function capitalizeWords(string) {
   return string.replace(/\b\w/g, (first) => first.toUpperCase());
 }
 
+// function capitalizeWords(string) {
+//   return string.split(' ')
+//   .map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+// }
+
 function displayBook(book){
   let ul = document.querySelector("ul")
   let li = document.createElement("li")
 
-  let author = document.querySelector("#author")
-  let authorValue = author.value
-  let authorCapital = capitalizeWords(authorValue)
+//   let author = document.querySelector("#author")
+//   let authorValue = author.value
+//   let authorCapital = capitalizeWords(authorValue)
     
-  li.classList.add("book")
-    
-  const {title, authors, publisher} = book.volumeInfo
+
+li.classList.add("book")
+
+const {title, authors, publisher} = book.volumeInfo
+
+  let authorArr = authors[0].split(" ")
+  let newAuthArr = [authorArr[0], authorArr[authorArr.length -1]]
+  let newAuthStr = (newAuthArr.join(" ")).replace(/ /g, "_")
 
   let publisherAlt = "Unknown"
   if (book.volumeInfo.hasOwnProperty("publisher")) {
@@ -55,10 +111,11 @@ function displayBook(book){
   }
     
   li.innerHTML += `<h2>${title}</h2>
-  <h3>Author: ${authors}<h3>
-  <h3>Publisher: ${publisherAlt}<h3>
-  <h3>Wiki: <a href="https://en.wikipedia.org/wiki/${authorCapital}">Author Wiki</a><h3>
+  <h3>Author: ${authors}</h3>
+  <h3>Publisher: ${publisherAlt}</h3>
+  <h3>Wiki: <a href="https://en.wikipedia.org/wiki/${newAuthStr}">Author Wiki</a></h3>
   <br>`
 
   ul.append(li)
+  
 }
