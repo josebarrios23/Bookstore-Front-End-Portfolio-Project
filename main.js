01658bd467cdf1c398307ee281fb77d7a67e3f44
Jose Barrios <josebarrios@pursuit.org>
2023-11-18 15:38:02 -0500

    added basic functionality to main.js

---
+++ main.js
@@ -0,0 +1,55 @@
+let form = document.querySelector("form")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    
+    
+    let title = document.querySelector("#title")
+    let titleValue = title.value
+    let titleConversion = titleValue ? titleValue.
+    //SUPER USEFUL "/ /g"
+    replace(/ /g, "+") : "*"
+    
+    let author = document.querySelector("#author")
+    let authorValue = author.value
+    let authorConversion = authorValue ? authorValue.replace(/ /g, "+") : "*"
+    
+    let ammount = document.querySelector("#ammount")
+    let maxResults = ammount.value
+    
+    // fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${titleConversion}&maxResults=${maxResults}&key=${API_KEY}`)
+    
+    //FURTHER INSPECTION:
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${authorConversion}+intitle:${titleConversion}&maxResults=${maxResults}&key=${API_KEY}`)
+    
+    .then((response) => response.json())
+    .then((data) => data.items.forEach((book) => displayBook(book)))
+    
+})
+
+let empty = document.querySelector("#empty")
+let bookshelf = document.querySelector("#bookshelf")
+empty.addEventListener("click", () => {
+    bookshelf.innerHTML = ""
+})
+
+function displayBook(book){
+  let ul = document.querySelector("ul")
+  let li = document.createElement("li")
+    
+  li.classList.add("book")
+    
+  const {title, authors, publisher} = book.volumeInfo
+
+  let publisherAlt = "Unknown"
+  if (book.volumeInfo.hasOwnProperty("publisher")) {
+    publisherAlt = publisher
+  }
+    
+  li.innerHTML += `<h2>${title}</h2>
+  <h3>Author: ${authors}<h3>
+  <h3>Publisher: ${publisherAlt}<h3>
+  <br>`
+
+  ul.append(li)
+}
