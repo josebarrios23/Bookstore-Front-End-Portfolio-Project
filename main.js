5ac70d01bb96a88e126a53ee6513986c174ddb7d
Jose Barrios <josebarrios@pursuit.org>
2023-11-20 02:36:49 -0500

    removed unneccesary let variables and organized code

--- main.js
+++ main.js
@@ -29,188 +29,162 @@ function loadRandomBooks() {
         "mathematics",
         "economics",
         "politics",
-      ]
+    ];
 
-    const randomIndex = Math.floor(Math.random() * randomSearchTerms.length)
-    const randomTerm = randomSearchTerms[randomIndex]
+    const randomIndex = Math.floor(Math.random() * randomSearchTerms.length);
+    const randomTerm = randomSearchTerms[randomIndex];
 
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${randomTerm}&maxResults=5&key=${API_KEY}`)
-    .then((response) => response.json())
-    .then((data) => {
-        data.items.forEach((book) => displayBook(book))
-      })
-      .catch((error) => displayError(error))
+        .then((response) => response.json())
+        .then((data) => {
+            data.items.forEach((book) => displayBook(book));
+        })
+        .catch((error) => displayError(error));
 }
 
-loadRandomBooks()
+loadRandomBooks();
 
 function displayError(error) {
-    const section = document.querySelector("section.error")
-    section.style.display = "block"
-  
+    const section = document.querySelector("section.error");
+    section.style.display = "block";
+
     section.innerHTML = `
     <p>Something went wrong!</p>
-    <p class="error-message">(${error})</p>`
+    <p class="error-message">(${error})</p>`;
 }
 
-/* <p>Please provide a "Book Title" or "Author Name".</p> */
-
-let form = document.querySelector("form")
+let form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const errorSection = document.querySelector("section.error")
-    errorSection.style.display = "none"
+    const errorSection = document.querySelector("section.error");
+    errorSection.style.display = "none";
 
     let bookshelf = document.querySelector("#bookshelf");
-    bookshelf.innerHTML = ""
-    
-    let title = event.target.elements["title"]
-    let titleValue = title.value
-    let titleConversion = titleValue ? titleValue.
-    //SUPER USEFUL "/ /g"
-    replace(/ /g, "+") : "*"
-    
-    let author = event.target.elements["author"]
-    let authorValue = author.value
-    let authorConversion = authorValue ? authorValue.replace(/ /g, "+") : "*"
-    
-    let amount = event.target.elements["amount"]
-    let maxResults = amount.value
-    
-    // fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${titleConversion}&maxResults=${maxResults}&key=${API_KEY}`)
-    
-    //FURTHER INSPECTION:
+    bookshelf.innerHTML = "";
+
+    const title = event.target.elements["title"];
+    const titleValue = title.value;
+    const titleConversion = titleValue ? titleValue.replace(/ /g, "+") : "*";
+
+    const author = event.target.elements["author"];
+    const authorValue = author.value;
+    const authorConversion = authorValue ? authorValue.replace(/ /g, "+") : "*";
+
+    const amount = event.target.elements["amount"];
+    const maxResults = amount.value;
+
     fetch(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${authorConversion}+intitle:${titleConversion}&maxResults=${maxResults}&key=${API_KEY}`)
-    
-    .then((response) => response.json())
-    .then((data) => data.items.forEach((book) => displayBook(book)))
-    .catch((error) => {
-        if (maxResults > 40) {
-            displayError(`Max Book Limit Reached (40 Books) -- ${error}`)}
-        else {
-            displayError(`Please add Author Name or Book Title -- ${error}`)
-        }})
-    
-})
+        .then((response) => response.json())
+        .then((data) => data.items.forEach((book) => displayBook(book)))
+        .catch((error) => {
+            if (maxResults > 40) {
+                displayError(`Max Book Limit Reached (40 Books) -- ${error}`);
+            } else {
+                displayError(`Please add Author Name or Book Title -- ${error}`);
+            }
+        });
+});
 
 function emptyBookshelf() {
-const errorSection = document.querySelector("section.error")
-let empty = document.querySelector("#empty")
-let bookshelf = document.querySelector("#bookshelf")
-empty.addEventListener("click", () => {
-    errorSection.style.display = "none"
-    bookshelf.innerHTML = ""
-})
+    const errorSection = document.querySelector("section.error");
+    const empty = document.querySelector("#empty");
+    const bookshelf = document.querySelector("#bookshelf");
+    empty.addEventListener("click", () => {
+        errorSection.style.display = "none";
+        bookshelf.innerHTML = "";
+    });
 }
-emptyBookshelf()
+emptyBookshelf();
 
-// function capitalizeWords(string) {
-//   return string.replace(/\b\w/g, (first) => first.toUpperCase());
-// }
+function displayBook(book) {
+    const ul = document.querySelector("ul");
+    const li = document.createElement("li");
 
-// function capitalizeWords(string) {
-//   return string.split(' ')
-//   .map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
-// }
+    li.classList.add("book");
 
-function displayBook(book){
-  let ul = document.querySelector("ul")
-  let li = document.createElement("li")
+    const { title, authors, publisher, description, imageLinks, categories } = book.volumeInfo;
 
-//   let author = document.querySelector("#author")
-//   let authorValue = author.value
-//   let authorCapital = capitalizeWords(authorValue)
+    let publisherAlt = "Unknown";
+    if (book.volumeInfo.hasOwnProperty("publisher")) {
+        publisherAlt = publisher;
+    }
 
-li.classList.add("book")
+    let authorsAlt = "Unknown";
+    if (book.volumeInfo.hasOwnProperty("authors")) {
+        authorsAlt = authors;
+    }
 
-const {title, authors, publisher, description, imageLinks, categories} = book.volumeInfo
+    let genresAlt = "Unknown";
+    if (book.volumeInfo.hasOwnProperty("categories")) {
+        genresAlt = categories;
+    }
 
-  
-  let publisherAlt = "Unknown"
-  if (book.volumeInfo.hasOwnProperty("publisher")) {
-    publisherAlt = publisher
-  }
+    let descriptionAlt = "Unavailable";
+    if (book.volumeInfo.hasOwnProperty("description")) {
+        descriptionAlt = description;
+    }
 
-  let authorsAlt = "Unknown"
-  if (book.volumeInfo.hasOwnProperty("authors")) {
-    authorsAlt = authors
-  }
+    li.innerHTML += `
+    <br>
+    <img src="${imageLinks && imageLinks.thumbnail ? imageLinks.thumbnail : 'noBook.png'}" alt="${title}"></img>
+    <h3>"${title}"</h3>
+    <button id="add"> Add To Bookshelf</button>
+    <h3>Author(s): ${authorsAlt}</h3>
+    <h3>Genre(s): ${genresAlt}</h3>
+    <h3>Publisher: ${publisherAlt}</h3>
+    <p class="hidden description">${descriptionAlt}</p>
+    <button value="show" id="unhide">Description</button>
+    <br>`;
 
-  let genresAlt = "Unknown"
-  if (book.volumeInfo.hasOwnProperty("categories")) {
-    genresAlt = categories
-  }
+    if (book.volumeInfo.hasOwnProperty("authors")) {
+        authors.forEach((author) => {
+            const authorModified = author.replace(/ /g, "+");
 
-  let descriptionAlt = "Unavailable"
-  if (book.volumeInfo.hasOwnProperty("description")) {
-    descriptionAlt = description
-  }
-    
-{/* <img src="${imageLinks && imageLinks.thumbnail ? imageLinks.thumbnail : 'placeholder-image.jpg'}" alt="${title}"></img> */}
+            li.innerHTML += `<h3>Wiki: <a href="https://en.wikipedia.org/w/index.php?search=${authorModified}" target="_blank">${author}</a></h3>`;
+        });
+    }
 
-  li.innerHTML += `
-  <br>
-  <img src="${imageLinks && imageLinks.thumbnail ? imageLinks.thumbnail : 'noBook.png'}" alt="${title}"></img>
-  <h3>"${title}"</h3>
-  <button id="add"> Add To Bookshelf</button>
-  <h3>Author(s): ${authorsAlt}</h3>
-  <h3>Genre(s): ${genresAlt}</h3>
-  <h3>Publisher: ${publisherAlt}</h3>
-  <p class="hidden description">${descriptionAlt}</p>
-  <button value="show" id="unhide">Show Description</button>
-  <br>`
-
-  if (book.volumeInfo.hasOwnProperty("authors")) {
-authors.forEach((author) => {
-  let authorModified = author.replace(/ /g, "+")
-
-  li.innerHTML += `<h3>Wiki: <a href="https://en.wikipedia.org/w/index.php?search=${authorModified}" target="_blank">${author}</a></h3>`
-})
-  }
-
-  ul.append(li)
-  
+    ul.append(li);
 }
 
-let addBook = document.querySelector("#add")
-let myBooks = document.querySelector("#my-books")
+const addBook = document.querySelector("#add");
+const myBooks = document.querySelector("#my-books");
 document.addEventListener("click", (event) => {
     if (event.target.id === "add") {
-        let closestLi = event.target.closest(".book");
-        let newLi = document.createElement("li")
-        newLi.classList.add("new-book")
-        newLi.innerHTML = closestLi.innerHTML
+        const closestLi = event.target.closest(".book");
+        const newLi = document.createElement("li");
+        newLi.classList.add("new-book");
+        newLi.innerHTML = closestLi.innerHTML;
         myBooks.append(newLi);
 
-        let bookshelfButton = newLi.querySelector("#add");
-            if (bookshelfButton) {
-                bookshelfButton.remove();
-            }
-        
-        let removeButton = document.createElement("button")
-        removeButton.id = "remove-book"
-        removeButton.innerText = "Remove Book"
-        newLi.append(removeButton)
+        const bookshelfButton = newLi.querySelector("#add");
+        if (bookshelfButton) {
+            bookshelfButton.remove();
+        }
+
+        const removeButton = document.createElement("button");
+        removeButton.id = "remove-book";
+        removeButton.innerText = "Remove Book";
+        newLi.append(removeButton);
     }
 });
 
-let removeBook = document.querySelector("#remove-book")
+const removeBook = document.querySelector("#remove-book");
 document.addEventListener("click", (event) => {
     if (event.target.id === "remove-book") {
-        let closestLi = event.target.closest(".new-book");
-        closestLi.remove()
+        const closestLi = event.target.closest(".new-book");
+        closestLi.remove();
     }
 });
 
-let unhideDescrip = document.querySelector("#unhide")
-let descrip = document.querySelector(".hidden")
+const unhideDescrip = document.querySelector("#unhide");
+const descrip = document.querySelector(".hidden");
 document.addEventListener("click", (event) => {
-  if (event.target.id === "unhide") {
-    const listItem = event.target.closest('li');
-    const descrip = listItem.querySelector('.description');
-        descrip.classList.toggle('hidden')
-  }
-})
-
+    if (event.target.id === "unhide") {
+        const listItem = event.target.closest('li');
+        const descrip = listItem.querySelector('.description');
+        descrip.classList.toggle('hidden');
+    }
+});
